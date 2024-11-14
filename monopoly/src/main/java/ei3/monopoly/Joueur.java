efd6b50cd70b67f5bfe142d15d7909bec8edd80e
DhaferG <dhaferghanmi.dg13@gmail.com>
2024-11-14 12:04:10 +0100

    Conflict resolution joueur

--- monopoly/src/main/java/ei3/monopoly/Joueur.java
+++ monopoly/src/main/java/ei3/monopoly/Joueur.java
@@ -34,6 +34,32 @@ public class Joueur {
      */
     
     public Joueur() {
+        this.nom="Bob";
+        this.pos=null;
+        this.fortune = 100000; // Fortune initialisée par défaut à 100000 €
+    }
+    private Case pos; 
+    private Plateau plateau;
+    
+    /**
+     * constructeur joueur
+     * @param nom : nom du joueur
+     * @param plateau
+     * @param pos : position du joueur
+     * @author nekouki
+     */
+    public Joueur(String nom, Plateau plateau, Case pos) {
+        this.nom = nom;
+        this.plateau = plateau;
+        this.pos = pos;
+        this.fortune = 100000; // Fortune initiale de 100000 €
+    }
+    /**
+     * constructeur par defaut
+     * @author nekouki
+     */
+    
+    public Joueur() {
         this.nom='Bob';
         this.pos=0;
         this.fortune = 100000; // Fortune initialisée par défaut à 100000 €
@@ -55,11 +81,15 @@ public class Joueur {
         this.fortune = fortune;
     }
     
+    public Plateau getPlateau() {
+        return this.plateau;
+    }
+    
+    /**
     /**
     * Afficher les infos du joueur
     * @author grigm
     */
-    @Override
     public void toString(){
         System.out.println("Nom du joueur : " + this.nom + "\nFortune : " + this.fortune); 
     }
@@ -69,6 +99,7 @@ public class Joueur {
     /**
     * 
     * @author grigm
+     * @return count le nombre de compagnies que possèdent le joueur 
      * @return count le nombre de compagnies que possèdent le joueur 
     */
     public int nbGares(){
@@ -82,7 +113,19 @@ public class Joueur {
           */  
         }
         return count; 
+    public int nbGares(){
+        int count = 0; 
+        for (int i = 0; i<40; i++){
+            /*if (this.plateau.getplateau().get(i) instanceof Achetable){
+              if (this.plateau.getplateau().get(i).getProp().getNom()== this.getNom()) {
+                    count ++; 
+                }  
+            }
+          */  
+        }
+        return count; 
     }
+    
     
     
     /**
@@ -105,9 +148,73 @@ public class Joueur {
 
     /**
     * Modifier le propriétaire de la case a et payer le prix de la case
+     *
+     * @author grigm
+     * @return count le nombre de compagnies que possèdent le joueur 
+     */    
+    public int nbCompanies(){
+        int count = 0; 
+        for (int i = 0; i<40; i++){
+            /*if (this.plateau.getplateau().get(i) instanceof Companies){
+              if (this.plateau.getplateau().get(i).getgetProp().getNom()== this.getNom()) {
+                    count ++; 
+                }  
+            }
+        */    
+        }
+        return count; 
+    }
+
+    /**
+    * Modifier le propriétaire de la case a et payer le prix de la case
     * @author grigm
      * @param a élément achetable à acheter 
+     * @param a élément achetable à acheter 
     */
+    public void acheter(Achetable a){
+         a.acheter(this);
+         
+         //on paie le prix de l'achat 
+         this.fortune = this.fortune - a.getPrix(); 
+         
+    }
+    
+    
+    /**
+    * Récupérer le prix de la case qu'on hypothèque
+    * @author grigm
+     * @param a case à hypotequer
+    */
+    public void hypotequer(Achetable a){
+        //on récupère le montant de la case 
+         this.fortune = this.fortune + a.getPrix(); 
+          
+    }
+    
+      /**
+    * Méthode pour construire une maison ou un immeuble
+    * @author grigm
+     * @param c case où construire une maison
+    */
+    public void construire(Constructible c){
+       
+        // on vérifie si on a assez d'argent 
+        if (c.getprixConstruction() > this.fortune){
+            System.out.println ("Vous n'avez pas assez d'argent pour construire"); 
+        } else {
+            // on paie la construction
+            this.fortune = this.fortune - c.getprixConstruction(); 
+            // on vérifie le nombre de maisons sur la case, moins de 4, on rajoute une maison 
+            if (c.getnbMaison()<=4){
+                c.setnbMaison(c.getnbMaison()+1) ; 
+            } // 4 maisons on met un immeuble et on enlève les maisons
+            else {
+                 c.setnbMaison(0) ; 
+                 c.setnbHotel(c.getnbHotel()+1);  
+            }     
+        }   
+    }
+
     public void acheter(Achetable a){
          a.acheter(this);
          
@@ -148,9 +255,30 @@ public class Joueur {
      * lancer de dé
      * @return entier entre 1 et 6
      */
+    /**
+     * lancer de dé
+     * @return entier entre 1 et 6
+     */
     public static int lanceLeDe() {
         return ((int) Math.floor(Math.random()*6))+1;
     }
+    
+    /**
+     * @param d: entier issu du dé
+     * @author: nekouki
+     */
+    
+    public void avancer(int d) {
+        // calcul de la nouvelle position
+        int nouvellePosition = (pos.getPosition() + d) % 40;
+        // Mettre à jour la position de la case
+        pos.setPosition(nouvellePosition);
+        
+    }
+    /**
+     * Tour de Jeu
+     * @author nekouki
+     */
     
     /**
      * @param d: entier issu du dé
@@ -170,6 +298,33 @@ public class Joueur {
     public void tourDeJeu() {
         // Lancer du dé pour avoir un nombre aléatoire entre 1 et 6
         int de= lanceLeDe();
+        // Avancer le joueur sur le plateau
+        avancer(de);
+        // 
+        if (pos instanceof Speciale) {
+            // Appliquer l'effet de la case spéciale
+            ((Speciale) pos).appliquerEffet();
+            
+        } else if (pos instanceof Achetable) {
+            Achetable caseAchetable = (Achetable) pos;
+            if (de % 2 == 1 && caseAchetable.getProprietaire() == null && fortune >= caseAchetable.getPrix()) {
+                try {
+                    acheter(caseAchetable);
+                } catch (NoMoreMoney e) {
+                    System.out.println(e.getMessage());
+                }
+            } else if (caseAchetable.getProprietaire() != null && caseAchetable.getProprietaire() != this) {
+                try {
+                    payer(caseAchetable.getProprietaire(), caseAchetable.calculerLoyer());
+                } catch (NoMoreMoney e) {
+                    System.out.println(nom + " ne peut pas payer le loyer et est éliminé.");
+                    plateau.elimination(this);
+                }
+            }
+        
+    
+        }
+    }   
         // Avancer le joueur sur le plateau
         avancer(de);
         // 
