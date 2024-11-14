b3782bcba504f57563256a82f7ce77f085f59299
Quentin.Bussard <quentin.bussard@eleves.ec-nantes.fr>
2024-11-14 11:31:19 +0100

    [feat] add all special cases

---
+++ monopoly/src/main/java/ei3/monopoly/Speciale.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ei3.monopoly;
+
+/**
+ *Toutes les case qui ne sont pas achetable qui ont des effets sur le jeu (prison, cartes chances,...)
+ * @author Quent
+ */
+public abstract class Speciale extends Case {
+
+    public Speciale(String nom) {
+        super(nom);
+    }
+
+    @Override
+    public String toString() {
+        return this.nom;
+    }
+    
+    public  abstract void appliquerEffet(Joueur j);
+}
