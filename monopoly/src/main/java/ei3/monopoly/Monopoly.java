35ac44fa1e96cd67d50b897f4d53a039c4643678
RBoutroux <remibtrx@gmail.com>
2024-11-14 08:22:15 +0100

    [feat] Creating netbeans project

---
+++ monopoly/src/main/java/ei3/monopoly/Monopoly.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ei3.monopoly;
+
+/**
+ *
+ * @author remib
+ */
+public class Monopoly {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
